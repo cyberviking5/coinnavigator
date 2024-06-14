78301898ac53b0399384abdb35a837446cace301
cyberviking5 <aviral.hatwal@gmail.com>
2024-06-15 01:53:57 +0530

    header done

--- src/components/Common/Header/index.js
+++ src/components/Common/Header/index.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Button from "../Button";
+import TemporaryDrawer from "./drawer";
 import "./styles.css";
+import Switch from "@mui/material/Switch";
+
 function Header() {
   return (
-    <div>
-      Hi
+    <div className="header">
+      <h1>
+        CryptoTracker<span style={{ color: "var(--blue)" }}>.</span>
+      </h1>
+      <div className="links">
+        <a href="/">
+          <p className="link">Home</p>
+        </a>
+        <a href="/compare">
+          <p className="link">Compare</p>
+        </a>
+        <a href="/watchlist">
+          <p className="link">Watchlist</p>
+        </a>
+        <a href="/dashboard">
+          <Button text={"hi"} />
+        </a>
+      </div>
+      <div className="drawer-component">
+        <TemporaryDrawer />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
---
+++ src/components/LandingPage/MainComponent/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./styles.css"
+function MainComponent() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MainComponent
