d02bec7c9b13ba967b594c75662571d70a9cb635
cyberviking5 <aviral.hatwal@gmail.com>
2024-06-15 00:34:19 +0530

    initial setup

---
+++ src/components/Common/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./styles.css";
+function Footer() {
+  return (
+    <div>
+      Bye
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Common/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./styles.css";
+function Header() {
+  return (
+    <div>
+      Hi
+    </div>
+  )
+}
+
+export default Header
