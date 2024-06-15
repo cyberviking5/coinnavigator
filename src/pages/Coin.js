67ab31a884ff1bfead0c121d92d1f3635daa298a
cyberviking5 <aviral.hatwal@gmail.com>
2024-06-15 19:51:29 +0530

    coin page done

--- src/pages/Coin.js
+++ src/pages/Coin.js
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Info from "../components/CoinPage/Info";
+import LineChart from "../components/CoinPage/LineChart";
+import SelectDays from "../components/CoinPage/SelectDays";
+import ToggleComponents from "../components/CoinPage/ToggleComponent";
 import Button from "../components/Common/Button";
 import Header from "../components/Common/Header";
 import Loader from "../components/Common/Loader";
 import List from "../components/Dashboard/List";
-import Info from "../components/CoinPage/Info";
 import { getCoinData } from "../functions/getCoinData";
 import { getPrices } from "../functions/getPrices";
+import { settingChartData } from "../functions/settingChartData";
 import { settingCoinObject } from "../functions/settingCoinObject";
+
 function Coin() {
-    const { id } = useParams();
-    const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [chartData, setChartData] = useState({ labels: [], datasets: [{}] });
   const [coin, setCoin] = useState({});
   const [days, setDays] = useState(30);
   const [priceType, setPriceType] = useState("prices");
+
   useEffect(() => {
     if (id) {
       getData();
@@ -25,28 +32,76 @@ function Coin() {
     setLoading(true);
     let coinData = await getCoinData(id, setError);
     // console.log("Coin DATA>>>>", coinData);
-    // setCoin(coinData);
     settingCoinObject(coinData, setCoin);
-    // console.log(coin)
-    // console.log("dsdvasdva>>"+coin)
-    console.log("ajnaosjn>>"+coin.price_change_percentage_24h)
     if (coinData) {
       const prices = await getPrices(id, days, priceType, setError);
       if (prices) {
+        settingChartData(setChartData, prices);
         setLoading(false);
       }
     }
   };
+
+  const handleDaysChange = async (event) => {
+    setLoading(true);
+    setDays(event.target.value);
+    const prices = await getPrices(id, event.target.value, priceType, setError);
+    if (prices) {
+      settingChartData(setChartData, prices);
+      setLoading(false);
+    }
+  };
+
+  const handlePriceTypeChange = async (event) => {
+    setLoading(true);
+    setPriceType(event.target.value);
+    const prices = await getPrices(id, days, event.target.value, setError);
+    if (prices) {
+      settingChartData(setChartData, prices);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      <Header/>
-      {loading?(<div>"hi"</div>):(<>
-       <div className="grey-wrapper">
+    <>
+      <Header />
+      {!error && !loading && coin.id ? (
+        <>
+          <div className="grey-wrapper">
             <List coin={coin} delay={0.5} />
-          </div> 
-           <Info title={coin.name} desc={coin.desc} /> </>)}
-    </div>
-  )
+          </div>
+          <div className="grey-wrapper">
+            <SelectDays handleDaysChange={handleDaysChange} days={days} />
+            <ToggleComponents
+              priceType={priceType}
+              handlePriceTypeChange={handlePriceTypeChange}
+            />
+            <LineChart chartData={chartData} />
+          </div>
+          <Info title={coin.name} desc={coin.desc} />
+        </>
+      ) : error ? (
+        <div>
+          <h1 style={{ textAlign: "center" }}>
+            Sorry, Couldn't find the coin you're looking for 😞
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              margin: "2rem",
+            }}
+          >
+            <a href="/dashboard">
+              <Button text="Dashboard" />
+            </a>
+          </div>
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </>
+  );
 }
 
-export default Coin
+export default Coin;
