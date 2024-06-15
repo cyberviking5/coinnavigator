7204f3e6b927c72f9297d32dd76a32e2f1095032
cyberviking5 <aviral.hatwal@gmail.com>
2024-06-15 14:34:28 +0530

    adding pagenation

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,12 +2,21 @@ import React, { useEffect, useState } from "react";
 import Header from '../components/Common/Header'
 import TabsComponent from '../components/Dashboard/Tabs'
 import axios from "axios";
+import Search from "../components/Dashboard/Search";
+import PaginationComponent from "../components/Dashboard/Pagination";
 
 function Dashboard() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+  const [paginatedCoins, setPaginatedCoins] = useState([]);
   useEffect(() => {
     getData();
   }, []);
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+    console.log(e.target.value);
+  };
 
   const getData = () => {
     axios
@@ -17,16 +26,35 @@ function Dashboard() {
       .then((response) => {
         console.log(response.data);
         setCoins(response.data);
+        setPaginatedCoins(response.data.slice(0,10)) ;
       })
       .catch((error) => {
         console.log(error.message);
       });
+      console.log(paginatedCoins)
   };
-
+  var filteredCoins = coins.filter(
+    (coin) =>
+      coin.name.toLowerCase().includes(search.trim().toLowerCase()) ||
+    coin.symbol.toLowerCase().includes(search.trim().toLowerCase())
+  );
+  const handlePageChange = (event, value) => {
+    setPage(value);
+    var initialCount = (value - 1) * 10;
+    setPaginatedCoins(coins.slice(initialCount, initialCount + 10));
+  };
   return (
     <div>
       <Header/>
-      <TabsComponent coins={coins}/>
+      <Search search={search} handleChange={handleChange}/>
+      <TabsComponent coins={search ? filteredCoins : paginatedCoins}/>
+      {!search && (
+            <PaginationComponent
+              page={page}
+              handlePageChange={handlePageChange}
+            />
+          )}
+
     </div>
   )
 }
