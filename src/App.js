c5463f78dbcd6ca05f2623d96a08e9e4a183d8dc
cyberviking5 <aviral.hatwal@gmail.com>
2024-06-15 22:13:07 +0530

    compare page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Dashboard from './pages/Dashboard';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Coin from './pages/Coin';
+import Compare from './pages/Compare';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/coin/:id" element={<Coin />} />
+            <Route path="/compare" element={<Compare />} />
           </Routes>
         </BrowserRouter>
 
