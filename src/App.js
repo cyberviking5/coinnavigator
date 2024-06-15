b12add5b795d367741a374eff338f1c5f2319ae0
cyberviking5 <aviral.hatwal@gmail.com>
2024-06-15 15:30:10 +0530

    loading added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Dashboard from './pages/Dashboard';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Coin from './pages/Coin';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/coin/:id" element={<Coin />} />
           </Routes>
         </BrowserRouter>
 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,10 +4,13 @@ import TabsComponent from '../components/Dashboard/Tabs'
 import axios from "axios";
 import Search from "../components/Dashboard/Search";
 import PaginationComponent from "../components/Dashboard/Pagination";
+import Loader from "../components/Common/Loader";
+import TopButton from "../components/Common/TopButton";
 
 function Dashboard() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [paginatedCoins, setPaginatedCoins] = useState([]);
   useEffect(() => {
@@ -19,6 +22,7 @@ function Dashboard() {
   };
 
   const getData = () => {
+    setLoading(true);
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
@@ -27,11 +31,11 @@ function Dashboard() {
         console.log(response.data);
         setCoins(response.data);
         setPaginatedCoins(response.data.slice(0,10)) ;
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error.message);
       });
-      console.log(paginatedCoins)
   };
   var filteredCoins = coins.filter(
     (coin) =>
@@ -46,6 +50,9 @@ function Dashboard() {
   return (
     <div>
       <Header/>
+      <TopButton /> 
+      {loading?(<Loader/>):(
+        <>
       <Search search={search} handleChange={handleChange}/>
       <TabsComponent coins={search ? filteredCoins : paginatedCoins}/>
       {!search && (
@@ -53,7 +60,7 @@ function Dashboard() {
               page={page}
               handlePageChange={handlePageChange}
             />
-          )}
+          )}</>)}
 
     </div>
   )
